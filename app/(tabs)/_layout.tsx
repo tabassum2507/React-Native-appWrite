73ba4ed24dbb92d916e75217f686ac62d384ddc9
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2024-07-01 14:53:14 +0530

    expo installed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,34 +4,47 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    // <Tabs
+    //   screenOptions={{
+    //     tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    //     headerShown: false,
+    //   }}>
+    //   <Tabs.Screen
+    //     name="index"
+    //     options={{
+    //       title: 'Home',
+    //       tabBarIcon: ({ color, focused }) => (
+    //         <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+    //       ),
+    //     }}
+    //   />
+    //   <Tabs.Screen
+    //     name="explore"
+    //     options={{
+    //       title: 'Explore',
+    //       tabBarIcon: ({ color, focused }) => (
+    //         <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+    //       ),
+    //     }}
+    //   />
+    // </Tabs>
+    <View style={styles.container}>
+      <Text>PootLayout</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container : {
+    display: 'flex',
+    flex : 1,
+    alignItems : 'center',
+    justifyContent : 'center'
+  }
+})
